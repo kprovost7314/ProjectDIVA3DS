54425b2adde3cfce55432f8530a7d1af5b0d63f1
Kaleb Provost <kalebprovost@gmail.com>
2017-01-05 15:10:14 -0600

    Comment out Citro3D initialization

--- source/main.h
+++ source/main.h
@@ -5,6 +5,7 @@
 #include <3ds.h>
 #include <citro3d.h>
 
+/*
 C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
 	C3D_RenderTarget* target = C3D_RenderTargetCreate(240, 400, GPU_RB_RGB8, GPU_RB_DEPTH24_STENCIL8);
 	C3D_RenderTargetSetClear(target, C3D_CLEAR_ALL, 0xFF0505, 0);
@@ -16,5 +17,6 @@ C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
 		GX_TRANSFER_SCALING(GX_TRANSFER_SCALE_XY);
 
 	C3D_RenderTargetSetOutput(target, GFX_TOP, GFX_LEFT, transferFlags);
+*/
 
 #endif // main_H_
