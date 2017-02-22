663b241b38f754f8d04c5b0ec66836fa7fb55ad0
Kaleb Provost <kalebprovost@gmail.com>
2017-02-22 13:34:37 -0600

    Update main.c

--- source/main.c
+++ source/main.c
@@ -4,18 +4,17 @@ int main(int argc, char **argv)
 {
 	gfxInitDefault();
 
+	printf("Project DIVA 3DS Homebrew\n");
+	printf("Press A to start game (will currently exit)\n");
+	printf("Press START to exit homebrew\n");
+	
 	while (aptMainLoop())
 	{
 		hidScanInput();
 
 		u32 kDown = hidKeysDown();
-		
-		printf("Project DIVA 3DS Homebrew\n");
-		printf("Press A to start game (will currently exit)\n");
-		printf("Press START to exit homebrew\n");
 
 		if (kDown & KEY_START) break;
-		if (kDown & KEY_A) startMenu();
 
 		gfxFlushBuffers();
 		gfxSwapBuffers();
